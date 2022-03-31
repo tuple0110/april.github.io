c71e2bb4106960b3cd1878eb9a8aaa68cd34b672
tuple0110 <bwelk123@gmail.com>
2022-03-31 21:11:11 +0900

    asdf

---
+++ index.js
@@ -0,0 +1,30 @@
+$file = document.getElementById("background");
+$image = document.getElementById("image");
+let fileAdded = false;
+$file.addEventListener("change", () => {
+  console.log("yee")
+  fileAdded = true;
+  $file.style.display = "none";
+  $image.style.display = "block";
+  $image.src = URL.createObjectURL($file.files[0])
+  document.body.style.overflow = "hidden";
+});
+imageList = [];
+index = 0;
+$image.addEventListener("click", (e) => {
+  if (fileAdded) {
+    index++;
+    image = new Image(300, 200);
+    image.src = "https://thumbs.gfycat.com/ImpressiveFixedGermanshepherd-max-1mb.gif";
+    image.classList.add("dance");
+    image.style.left = `${e.clientX - 150}px`;
+    image.style.top = `${e.clientY - 100}px`;
+    image.id = index;
+    imageList.push(index);
+    document.body.appendChild(image);
+    setTimeout(() => {
+      document.getElementById(imageList[0]).remove();
+      imageList = imageList.slice(1);
+    }, 3000)
+  }
+});
